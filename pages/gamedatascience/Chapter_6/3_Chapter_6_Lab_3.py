3897ecda2a4319d485bb75b03894c6bb1452eec6
Thomas Tellner <tellner@hotmail.com>
2025-12-07 08:50:00 -0800

    Add files via upload

---
+++ pages/gamedatascience/Chapter_6/3_Chapter_6_Lab_3.py
@@ -0,0 +1,13 @@
+import streamlit as st
+from nbconvert import HTMLExporter
+import nbformat
+
+st.set_page_config(layout="wide")
+
+with open("D:\\Repos\\portfolio\\pages\\gamedatascience\\Chapter_6\\Chapter6_Lab3.ipynb", "r", encoding="utf-8") as f:
+    notebook = nbformat.read(f, as_version=4)
+
+html_exporter = HTMLExporter()
+(body, _) = html_exporter.from_notebook_node(notebook)
+
+st.components.v1.html(body, height=800, scrolling=True)
